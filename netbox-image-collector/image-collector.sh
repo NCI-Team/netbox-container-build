e032c1ab601c53958eba8b3a827b41c7c49b92ae
remchuk <133580699+remchuk@users.noreply.github.com>
2023-07-26 18:00:29 +0300

    Create image-collector.sh

---
+++ netbox-image-collector/image-collector.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+declare -A images=(
+["public.ecr.aws/docker/library/redis"]="7.0.5-alpine3.16"
+["public.ecr.aws/docker/library/haproxy"]="2.5.9-alpine3.16"
+["public.ecr.aws/bitnami/postgresql-repmgr"]="14.5.0-debian-11-r17"
+["docker.io/netboxcommunity/netbox"]="v3.5"
+)
+
+for key in "${!images[@]}" 
+do
+    podman pull -q "$key:${images[$key]}"
+done
+
+echo FROM haproxy > ./Containerfile
+echo USER root >> ./Containerfile
+echo RUN apk add postgresql bash >> ./Containerfile
+
+podman build -t haproxy-pg-bash:2.5.9-alpine3.16 .
+
+images["haproxy-pg-bash"]="${images["public.ecr.aws/docker/library/haproxy"]}"
+unset images["public.ecr.aws/docker/library/haproxy"]
+
+# Create the command for saving and removing images
+command="podman save -o netbox-images.tar"
+
+for key in "${!images[@]}"
+do
+  command+=" $key:${images[$key]}"
+done
+
+# Execute the command
+$command
