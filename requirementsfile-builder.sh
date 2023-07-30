418a77068944ffc7bf1034da75b660c7a4e339a5
remchuk <133580699+remchuk@users.noreply.github.com>
2023-07-30 14:49:42 +0300

    Create requirementsfile-builder.sh

---
+++ requirementsfile-builder.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p 'Python Package To Download: ' python_package
+podman build -t python-env-dev .
+podman run -dit --name=python_develop python-env-dev bash
+podman exec -it python_develop pip3 install $python_package
+podman exec -it python_develop bash -c "pip3 freeze > /tmp/requirements.txt"
+podman cp python_develop:/tmp/requirements.txt ./requirements2.txt
+podman rm -f python_devops 
