7c77f87d8202d8af63f70731c92bd3d4c9b7a98e
remchuk <133580699+remchuk@users.noreply.github.com>
2023-07-31 08:47:29 +0300

    Update requirementsfile-builder.sh

--- requirementsfile-builder.sh
+++ requirementsfile-builder.sh
@@ -5,5 +5,5 @@ podman build -t python-env-dev .
 podman run -dit --name=python_develop python-env-dev bash
 podman exec -it python_develop pip3 install $python_package
 podman exec -it python_develop bash -c "pip3 freeze > /tmp/requirements.txt"
-podman cp python_develop:/tmp/requirements.txt ./requirements2.txt
-podman rm -f python_devops 
+podman cp python_develop:/tmp/requirements.txt ./requirements.txt
+podman rm -f python_develop
