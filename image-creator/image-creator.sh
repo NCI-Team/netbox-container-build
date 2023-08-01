1a46b48e0f3acfc9864753f0eaf0cd4a2c6e3deb
remchuk <133580699+remchuk@users.noreply.github.com>
2023-08-01 10:18:48 +0300

    Update image-creator.sh

--- image-creator/image-creator.sh
+++ image-creator/image-creator.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 declare -A extensions=(
-["VisualStudioExptTeam"]="vscodeintellicode"
-["VisualStudioExptTeam"]="intellicode-api-usage-examples"
-["ms-python"]="vscode-pylance"
-["ms-vscode-remote"]="remote-containers"
+["vscode-pylance"]="ms-python"
+["vscodeintellicode"]="VisualStudioExptTeam"
+["intellicode-api-usage-examples"]="VisualStudioExptTeam"
+["remote-containers"]="ms-vscode-remote"
+["vsc-python-indent"]="KevinRose"
+["python"]="ms-python"
+["autodocstring"]="njpwerner"
+["python-environment-manager"]="donjayamanne"
+["python-extension-pack"]="donjayamanne"
+["vscode-django"]="batisteo"
+["prettier-vscode"]="esbenp"
+["jinja"]="wholroyd"
 )
 
 for key in "${!extensions[@]}" 
 do
-    wget -nv https://marketplace.visualstudio.com/_apis/public/gallery/publishers/$key/vsextensions/${extensions[$key]}/latest/vspackage
-    mv vspackage $key.${extensions[$key]}-latest.vsix.gz
-    gunzip -v $key.${extensions[$key]}-latest.vsix.gz
+
+    wget -nv -w 10 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/${extensions[$key]}/vsextensions/$key/latest/vspackage
+    mv vspackage ${extensions[$key]}.$key-latest.vsix.gz
+    gunzip -v ${extensions[$key]}.$key-latest.vsix.gz
 done
 
 podman build -t vscodeimage .
-
