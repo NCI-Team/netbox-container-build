9e1ad124a9117d7cc2cc26beb80441337745721a
Rem <m.remmarkov@gmail.com>
2023-08-29 15:09:03 +0300

    add haproxy var to image-colletor.sh

--- netbox-image-collector/image-collector.sh
+++ netbox-image-collector/image-collector.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+haproxy="public.ecr.aws/docker/library/haproxy"
+haproxy_tag="2.5.9-alpine3.16"
+
 declare -A images=(
 ["public.ecr.aws/docker/library/redis"]="7.0.5-alpine3.16"
-["public.ecr.aws/docker/library/haproxy"]="2.5.9-alpine3.16"
+[$haproxy]=$haproxy_tag
 ["public.ecr.aws/bitnami/postgresql-repmgr"]="14.5.0-debian-11-r17"
 ["docker.io/netboxcommunity/netbox"]="v3.5"
 )
@@ -11,14 +15,14 @@ do
     podman pull "$key:${images[$key]}"
 done
 
-echo FROM public.ecr.aws/docker/library/haproxy:2.5.9-alpine3.16 > ./Containerfile
+echo FROM $haproxy:$haproxy_tag > ./Containerfile
 echo USER root >> ./Containerfile
 echo RUN apk add postgresql bash >> ./Containerfile
 
-podman build --no-cache -t haproxy-pg-bash:2.5.9-alpine3.16 .
+podman build --no-cache -t haproxy-pg-bash:$haproxy_tag .
 
-images["haproxy-pg-bash"]="${images["public.ecr.aws/docker/library/haproxy"]}"
-unset images["public.ecr.aws/docker/library/haproxy"]
+images["haproxy-pg-bash"]="$haproxy_tag"
+unset images[$haproxy]
 
 # Create the command for saving and removing images
 command="podman save -o netbox-images.tar"
