a0df943b2da1518570286334e8c77f67feefaadf
Gil Mery <ylb.gil.mery@gmail.com>
2024-08-25 16:28:31 +0300

    add generator s3cmd create

---
+++ snmp-generator-creator/snmp-generator-image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum install gcc make net-snmp net-snmp-utils net-snmp-libs net-snmp-devel
+git clone https://github.com/prometheus/snmp_exporter.git
+cd snmp_exporter/generator
+make docker-generate
+podman build .
