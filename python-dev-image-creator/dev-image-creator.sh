33c0d3091e6185042bd2eea465245e52b47e4206
remchuk <133580699+remchuk@users.noreply.github.com>
2024-09-24 15:03:07 +0300

    Add ansible-vault

--- python-dev-image-creator/dev-image-creator.sh
+++ python-dev-image-creator/dev-image-creator.sh
@@ -19,6 +19,7 @@ declare -A extensions=(
     ["vscode-peacock"]="johnpapa"
     ["codescene-vscode"]="CodeScene"
     ["vscode-markdownlint"]="DavidAnson"
+    ["ansible-vault"]="dhoeric"
 )
 
 if [ ! -d "./extensions" ]; then
