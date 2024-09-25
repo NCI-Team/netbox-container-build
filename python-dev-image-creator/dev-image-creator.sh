edfcfd736aa000b7b8802c76771bb085696ad339
remchuk <133580699+remchuk@users.noreply.github.com>
2024-09-25 12:55:45 +0300

    yes

--- python-dev-image-creator/dev-image-creator.sh
+++ python-dev-image-creator/dev-image-creator.sh
@@ -17,7 +17,6 @@ declare -A extensions=(
     ["markdown-preview-github-styles"]="bierner"
     ["gitlens"]="eamodio"
     ["vscode-peacock"]="johnpapa"
-    ["codescene-vscode"]="CodeScene"
     ["vscode-markdownlint"]="DavidAnson"
 )
 
@@ -30,15 +29,17 @@ cd ./extensions
 for key in "${!extensions[@]}"
 do
     version="latest"
-    if [ $key -eq "python" ]; then
-        $version = "-2024.14.1"
+    if [ "$key" = "python" ]; then
+        version=$(echo "${version/latest/2024.14.1}")
     fi
     
     FILE=${extensions[$key]}.$key-$version.vsix
     while [ ! -f ./$FILE ];
     do
         publisher=${extensions[$key]}
-        
+    
+        echo $key
+        echo $version
         wget -nv -w 10 --random-wait --continue https://marketplace.visualstudio.com/_apis/public/gallery/publishers/$publisher/vsextensions/$key/$version/vspackage
         mv vspackage $FILE
     done
